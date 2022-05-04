83657da5adc0efc41950ec3062df52fb79614c71
LAPTOP-BQNGAEFP\graci <graciagarpin>
2022-05-04 23:37:03 +0200

    fix class name

--- src/components/MovieSceneCard.js
+++ src/components/MovieSceneCard.js
@@ -2,7 +2,7 @@ const MovieSceneCard = (props) => {
   return (
     <article className="card"> 
       <img className="card__poster" src={props.card.poster} alt={props.card.movie} title="card__movie poster" />
-      <h4 className="card__card__title">{props.card.movie} - {props.card.year}</h4>
+      <h4 className="card__title">{props.card.movie} - {props.card.year}</h4>
       <p className="card__quote">{props.card.full_line}</p>
     </article>
   );
