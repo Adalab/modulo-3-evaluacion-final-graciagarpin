e325f016dd813ff8c2db03380b2d7becd4523531
LAPTOP-BQNGAEFP\graci <graciagarpin>
2022-05-04 20:39:23 +0200

    create basic html structure input and select filter

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,31 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <header>
+        <h1>Owen Wilson's "WoW"</h1>
+      </header>
+      <main>
+        <form action="">
+          <label htmlFor="movieInput">Movie</label>
+          <input
+            type="text"
+            name="movieInput"
+            id="movieInput"
+            className="movieInput"
+            autoComplete="off"
+            placeholder="Little Fockers"
+          />
+          <label htmlFor="yearSelect">Year</label>
+          <select
+            type="select"
+            name="yearSelect"
+            id="yearSelect"
+            className="yearSelect"
+          >
+            <option> </option>
+          </select>
+        </form>
+      </main>
     </div>
   );
 }
