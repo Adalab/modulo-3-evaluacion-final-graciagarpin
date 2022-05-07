7720b2bf67bc0a8cd9291769e2862cef43c2b52e
LAPTOP-BQNGAEFP\graci <graciagarpin>
2022-05-07 12:17:16 +0200

    create detail component with html content

--- src/components/MovieSceneDetail.js
+++ src/components/MovieSceneDetail.js
@@ -1,7 +1,22 @@
 import '../styles/core/_reset.scss';
+import '../styles/layout/_detail.scss';
 
-const MovieSceneDetail = () => {
-
-}
+const MovieSceneDetail = (props) => {
+  console.log(props);
+  return (
+    <section className="detail">
+      <img
+        className="detail__poster"
+        src={props.scene.poster}
+        alt={props.scene.movie}
+        title="movie poster"
+      />
+      <h2 className="detail__movie">{props.scene.movie}</h2>
+      <p className="detail__info">{props.scene.full_line}</p>
+      <p className="detail__info">{props.scene.director}</p>
+      <audio src={props.scene.audio}></audio>
+    </section>
+  );
+};
 
 export default MovieSceneDetail;
