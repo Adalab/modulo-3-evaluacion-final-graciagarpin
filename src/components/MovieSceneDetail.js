1def5f9ecb73b82a745bc1be5c7435d96b642870
LAPTOP-BQNGAEFP\graci <graciagarpin>
2022-05-07 20:10:20 +0200

    create style to detail card responsive with grid

--- src/components/MovieSceneDetail.js
+++ src/components/MovieSceneDetail.js
@@ -4,20 +4,24 @@ import '../styles/layout/_detail.scss';
 const MovieSceneDetail = (props) => {
   console.log(props);
   return (
-    <section className="detail">
-      <img
-        className="detail__poster"
-        src={props.scene.poster}
-        alt={props.scene.movie}
-        title="movie poster"
-      />
-      <h2 className="detail__movie">{props.scene.movie}</h2>
-      <p className="detail__info">{props.scene.full_line}</p>
-      <p className="detail__info">{props.scene.director}</p>
-      <a className="detail__audioLink" href={props.scene.audio}>
-        Escuchar audio
-      </a>
-    </section>
+    <div className="page">
+      <section className="detail">
+        <img
+          className="detail__poster"
+          src={props.scene.poster}
+          alt={props.scene.movie}
+          title="movie poster"
+        />
+        <div className="detail__description">
+          <h2 className="description__movie">{props.scene.movie}</h2>
+          <p className="description__info">{props.scene.full_line}</p>
+          <p className="description__info">{props.scene.director}</p>
+          <a className="description__audioLink" href={props.scene.audio}>
+            Escuchar audio
+          </a>
+        </div>
+      </section>
+    </div>
   );
 };
 
