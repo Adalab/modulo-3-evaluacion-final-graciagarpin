16c02a825e9c13dabf6decca0e724071cf4c208e
LAPTOP-BQNGAEFP\graci <graciagarpin>
2022-06-22 19:38:04 +0200

    import styles files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import '../styles/core/_reset.scss';
+import '../styles/layout/_header.scss';
+
 const Header = () => {
-  
-return (<header>
-  <h1 className="title">Owen Wilson's "WoW"</h1>
-</header>);
+  return (
+    <header>
+      <h1 className="title">Owen Wilson's "WoW"</h1>
+    </header>
+  );
 };
 
 export default Header;
